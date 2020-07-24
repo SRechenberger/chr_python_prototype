d06a1bb8c092fe04414209c7bbc545aceea85571
Sascha Rechenberger <sascha.rechenberger@alumni.uni-ulm.de>
2020-07-24 12:05:57 +0200

    extended test

--- test/test_code_gen.py
+++ test/test_code_gen.py
@@ -15,7 +15,11 @@ def __gcd_1_1(id_0, _1):
     for id_1, c_1 in self.chr.get_iterator(symbol='gcd/1', fixed=True):
         if self.chr.alive(id_0) and self.chr.alive(id_1) and all_different(id_0, id_1):
             _0 = c_1[1]
-            if _0.get_value() <= _1.get_value():
+            if (
+                self.builtin.is_bound(_0) and
+                self.builtin.is_bound(_1) and
+                _0.get_value() <= _1.get_value()
+            ):
                 pass
     return False
 
@@ -23,7 +27,11 @@ def __gcd_1_2(id_1, _0):
     for id_0, c_0 in self.chr.get_iterator(symbol='gcd/1', fixed=True):
         if self.chr.alive(id_0) and self.chr.alive(id_1) and all_different(id_0, id_1):
             _1 = c_0[1]
-            if _0.get_value() <= _1.get_value():
+            if (
+                self.builtin.is_bound(_0) and
+                self.builtin.is_bound(_1) and
+                _0.get_value() <= _1.get_value()
+            ):
                 pass
     return False
 '''
@@ -43,7 +51,11 @@ program = Program(rules=[
         name="r2",
         kept_head=[Constraint("gcd", params=["_0"])],
         removed_head=[Constraint("gcd", params=["_1"])],
-        guard=[Constraint("ask_leq", params=[Var("_0"), Var("_1")])],
+        guard=[
+            Constraint("ask_bound", params=[Var("_0")]),
+            Constraint("ask_bound", params=[Var("_1")]),
+            Constraint("ask_leq", params=[Var("_0"), Var("_1")])
+        ],
         body=[
             Constraint("tell_eq", params=[Var("_2"), Term("-", [Var("_0"), Var("_1")])]),
             Constraint("gcd", params=[Var("_2")])
