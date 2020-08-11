1aea8dd63e162cf9bf27e9947c6a7deaf7bcef2f
Sascha Rechenberger <sascha.rechenberger@uni-ulm.de>
2020-08-11 09:30:04 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='chr-python',
+    version='20.0.1',
+    packages=['chr', 'test', 'test_files'],
+    url='',
+    license='MIT',
+    author='Sascha Rechenberger',
+    author_email='sascha.rechenberger@uni-ulm.de',
+    description='Constraint Handling Rules for Python'
+)
